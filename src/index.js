a9fed56cf9c0d3d9312be8510a0d5df16798d278
Ismael Coulibaly <i.coulibaly@groupeonepoint.com>
2019-06-25 15:46:07 +0200

    Reorganize imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+// import App from './App';
+import RuleList from './RuleList';
+
+import rules from './data.json'
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log(rules)
+
+// const domElement = document.getElementById('root');
+ReactDOM.render(<RuleList rules={rules} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
