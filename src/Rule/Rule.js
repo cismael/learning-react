52ec15ad883b983920406fb609d39f329226ef46
Ismael Coulibaly <i.coulibaly@groupeonepoint.com>
2019-06-25 16:41:15 +0200

    Tranform Render function to Class

--- src/Rule/Rule.js
+++ src/Rule/Rule.js
@@ -1,40 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './Rule.css'
 
 import PropTypes from 'prop-types';
 
-const Rule = ({title, description, likes, dislikes, tags}) => (
-        <div className="panel panel-primary">
-            <div className="panel-heading rule-title" role="presentation">
-                {title}
-                <i className="pull-right glyphicon glyphicon-chevron-down"></i>
-            </div>
-            <div className="panel-body">
-                {description}
-            </div>
-            <div className="panel-footer">
-                <div className="btn-toolbar">
-                    {tags.map(tag => (
-                        <span key={tag} className="badge">{tag}</span> 
-                    ))} 
-                <div className="btn-group btn-group-xs pull-right">
-                    <a className="btn btn-primary" title="Update">
-                        <i className="glyphicon glyphicon-pencil"></i>
-                    </a>
+class Rule extends Component {
+    render() {
+        const {title, description, likes, dislikes, tags} = this.props;
+        return (
+            <div className="panel panel-primary">
+                <div className="panel-heading rule-title" role="presentation">
+                    {title}
+                    <i className="pull-right glyphicon glyphicon-chevron-down"></i>
                 </div>
-                <div className="btn-group btn-group-xs pull-right">
-                    <a className="btn btn-default" title="+1">
-                        {likes} <i className="glyphicon glyphicon-thumbs-up"></i>   
-                    </a>
-                    <a className="btn btn-default" title="-1">
-                        {dislikes} <i className="glyphicon glyphicon-thumbs-down"></i>
-                    </a>
+                <div className="panel-body">
+                    {description}
                 </div>
+                <div className="panel-footer">
+                    <div className="btn-toolbar">
+                        {tags.map(tag => (
+                            <span key={tag} className="badge">{tag}</span> 
+                        ))} 
+                    <div className="btn-group btn-group-xs pull-right">
+                        <a className="btn btn-primary" title="Update">
+                            <i className="glyphicon glyphicon-pencil"></i>
+                        </a>
+                    </div>
+                    <div className="btn-group btn-group-xs pull-right">
+                        <a className="btn btn-default" title="+1">
+                            {likes} <i className="glyphicon glyphicon-thumbs-up"></i>   
+                        </a>
+                        <a className="btn btn-default" title="-1">
+                            {dislikes} <i className="glyphicon glyphicon-thumbs-down"></i>
+                        </a>
+                    </div>
+                    </div>
                 </div>
             </div>
-        </div>
-)
+        )
+    }
+}
 
 Rule.propTypes = {
     title: PropTypes.string,
