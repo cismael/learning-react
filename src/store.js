7ca3ddba7a8d20b128ef6bf3f325b2ab615c77df
Ismael Coulibaly <i.coulibaly@groupeonepoint.com>
2019-06-27 13:43:37 +0200

    Add redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,6 +1,10 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 
-import rulesReducer, { loadRules } from "./redux/rules";
+import rulesReducer from "./redux/rules";
+
+import thunkMiddleware from 'redux-thunk';
+
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 const reducer = combineReducers({
     rules: rulesReducer,
@@ -8,7 +12,11 @@ const reducer = combineReducers({
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeWithDevTools(
+        applyMiddleware(thunkMiddleware),
+        // other store enhancers if any
+    )
+    
 );
 
 export default store;
